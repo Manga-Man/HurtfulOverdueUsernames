829441838de93b6f29e6e500166d598ee1941431
Son of a glitch <72046004+Manga-Man@users.noreply.github.com>
2023-05-14 20:01:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-var width= window.innerWidth;
-console.log(width)
-
-var Elements = document.getElementById('break');
-
-setTimeout(() => {  fix() }, 2000);
-
-function fix() {
-if(width > 784) {
-  console.log('true')
-  Elements.remove();
-}
-}
-  
+console.log('live');
